9b9385d255364b33c190e2491f6b625dd0bb2377
Anthony Grimes <i@raynes.me>
2011-10-20 04:29:47 -0500

    Some bitly-specific tests.

--- test/hobbit/bitly_test.clj
+++ test/hobbit/bitly_test.clj
@@ -1,2 +1,11 @@
 (ns hobbit.bitly-test
-  (:use [hobbit core bitly]))
+  (:use [hobbit core bitly utils]
+        clojure.test))
+
+
+(def auth (:bitly (read-auth)))
+(def long "http://blog.raynes.me")
+
+(deftest shorten-test
+  (is (shorten (bitly-shortener (:key auth) (:login auth)) long))
+  (is (shorten (bitly-shortener (:key auth) (:login auth) "j.mp") long)))
