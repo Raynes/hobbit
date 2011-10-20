9acec6a3b37f0512a8438f13dac309580f1b8ec4
Anthony Grimes <i@raynes.me>
2011-10-20 01:14:07 -0500

    s/I/A/

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject hobbit "0.1.0"
-  :description "I library for interfacing with various URL shortening services."
+(defproject hobbit "0.1.0-SNAPSHOT"
+  :description "A library for interfacing with various URL shortening services."
   :dependencies [[clojure "1.3.0"]
                  [clj-http "0.2.1"]
                  [org.clojure/data.json "0.1.2"]])
