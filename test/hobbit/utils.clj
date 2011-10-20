0b67633709f157c4109acd87f0253cd718fa7c96
Anthony Grimes <i@raynes.me>
2011-10-20 16:34:16 -0500

    Added a few useful things.

--- test/hobbit/utils.clj
+++ test/hobbit/utils.clj
@@ -2,3 +2,10 @@
 
 (defn read-auth []
   (read-string (slurp "auth.clj")))
+
+(defn link? [s] (and (string? s) (.startsWith s "http")))
+
+(def some-chars "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVWXY1234567890")
+
+(defn random-string []
+  (apply str (repeatedly 20 #(rand-nth some-chars))))
