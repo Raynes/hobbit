8198c23bb83c699f51298e407aed4dfed461094b
Anthony Grimes <i@raynes.me>
2012-02-25 21:22:36 -0600

    Update deps and get rid of snapshot.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject hobbit "0.1.0-SNAPSHOT"
+(defproject hobbit "0.1.0-alpha1"
   :description "A library for interfacing with various URL shortening services."
-  :dependencies [[clojure "1.3.0"]
-                 [clj-http "0.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-http "0.3.2"]
                  [org.clojure/data.json "0.1.2"]])
